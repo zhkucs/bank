624012e77ae72332cc810cb6b1eb8e15cda96cbe
RICK <RICK@192.168.0.115>
2015-03-19 14:05:15 +0800

    新建

--- BankConsole/src/BankApp.java
+++ BankConsole/src/BankApp.java
@@ -1,9 +1,15 @@
 
 public class BankApp {
 	public static void main(String[] args){
+		//...登录
+		//开设一个账户
 		Account acc = new Account();
+		//1.存入500 元
 		acc.save(500.0);
+		//2.取出100 元
+		// 检查
 		acc.depoit(100.0);
+		//3.显示存取款的明细（操作类型，操作金额，操作时间，余额）
 		acc.history();
 	}
 }
